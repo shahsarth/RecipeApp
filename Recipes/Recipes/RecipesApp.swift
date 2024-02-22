a06f6c23ad6fb5ebcdff64148b9e70a6865786a8
Sarth Shah <shahsarth@gmail.com>
2024-02-22 09:22:47 +0530

    first project files

---
+++ Recipes/Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Sarth on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
