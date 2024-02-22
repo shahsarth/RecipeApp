4ab07aea678a4ce437c936244bcdc69c3beae0bd
Sarth Shah <shahsarth@gmail.com>
2024-02-22 09:32:54 +0530

    added basic data model for a Dessert

--- Recipes/Recipes/Models/Dessert.swift
+++ Recipes/Recipes/Models/Dessert.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class Dessert: ObservableObject, Identifiable, Equatable, Hashable {
+    var id : UUID
+    var name: String
+    var thumbnail: String
+    
+    init(id: UUID, name: String, thumbnail: String) {
+        self.id = id
+        self.name = name
+        self.thumbnail = thumbnail
+    }
+    
+    static func ==(lhs: Dessert, rhs: Dessert) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
