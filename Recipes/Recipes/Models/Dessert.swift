261492dc0ce9807618864130e524554c1afc6423
Sarth Shah <shahsarth@gmail.com>
2024-02-22 09:28:50 +0530

    Added basic MVVM folder structure

---
+++ Recipes/Recipes/Models/Dessert.swift
@@ -0,0 +1,8 @@
+//
+//  Dessert.swift
+//  Recipes
+//
+//  Created by Sarth on 2/22/24.
+//
+
+import Foundation
---
+++ Recipes/Recipes/ViewModels/MealViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  MealViewModel.swift
+//  Recipes
+//
+//  Created by Sarth on 2/22/24.
+//
+
+// Handle all API request logic here
+import Foundation
+
+
